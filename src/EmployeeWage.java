49cc01d1a972b7c6a4746391281e96a4d7f74bc8
sandeepPangare <pangaresandeep96@gmail.com>
2022-09-01 22:02:53 +0530

    Added:Emoloyeewage

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class EmployeeWage {
-}
+public class EmployeeWage {
+
+        public static void main(String[] args) {
+
+            int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+
+            if (empCheck == 1) {
+                System.out.println("Employee is present");
+            } else
+                System.out.println("Employee is absent");
+
+        }
+    }
